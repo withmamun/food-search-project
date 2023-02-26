c9e54c6bdbfd1b51f4830672fc8792515230163c
Mamun Mia <mamunahammedco59@gmail.com>
2023-02-26 18:31:49 +0600

    data import and modify

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,62 +1,38 @@
-(function ($) {
-    "use strict";
+const loadMeals = (searchText) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMeals(data.meals))
+}
+
+const displayMeals = meals => {
+    const mealContainer = document.getElementById('meal-container');
+    mealContainer.innerHTML = '';
+    meals.forEach(meal => {
+        console.log(meal);
+        const mealDiv = document.createElement('div');
+        mealDiv.classList.add('col-lg-6');
+        mealDiv.innerHTML = `
+        <div class="food__single__blk">
+            <div class="food__img__blk">
+                <img src="${meal.strMealThumb}" alt="">
+            </div>
+            <div class="food__single__content">
+                <h4>${meal.strMeal}</h4>
+                <p>There are many variations of passages of available, but the majority have suffered
+                </p>
+                <a href="#">View Details</a>
+            </div>
+        </div>
+        `;
+        mealContainer.appendChild(mealDiv);
+    });
+}
+
+const searchMeals = () => {
+    const searchText = document.getElementById('search-field').value;
+    loadMeals(searchText);
+}
 
 
-    jQuery(document).ready(function ($) {
-
-
-        //------------ Offcanvas -------------
-
-        $('.menu-open , .offcanvas-overlay').click(function () {
-
-            $('.offcanvas-area , .offcanvas-overlay').addClass('active');
-
-        });
-        $('.menu-close , .offcanvas-overlay').click(function () {
-
-            $('.offcanvas-area , .offcanvas-overlay').removeClass('active');
-
-        });
-
-
-        //------------ Hero slider -------------
-
-        $('.hero__area_slider').owlCarousel({
-            loop: true,
-            margin: 0,
-            nav: false,
-            dot: true,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                600: {
-                    items: 1
-                },
-                1000: {
-                    items: 1
-                }
-            }
-        })
-
-
-        //---owl dots number-----
-        
-        var i = 1;
-        
-        $('.hero-slier-main.owl-carousel .owl-dot').each(function () {
-            $(this).text(i);
-            i++;
-        });
-
-
-
-
-
-    }); //---document-ready-----
-
-
-
-
-
-}(jQuery));
+loadMeals('rice')
